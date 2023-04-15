6d1c0a0a689958e85936335239b3c7de727c65f1
DanielSanchez052 <sanchezd052@gmail.com>
2023-04-15 15:14:45 -0500

    Add new General configurations

--- NotificationApi/settings/base.py
+++ NotificationApi/settings/base.py
@@ -43,6 +43,7 @@ LOCAL_APPS = [
 
 THIRD_APPS = [
     "rest_framework",
+    'django_filters',
     "drf_yasg",
     "corsheaders",
     "import_export",
@@ -144,12 +145,17 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         # 'rest_framework.permissions.DjangoModelPermissions',
-        'rest_framework.permissions.IsAuthenticated',
-        # 'rest_framework.permissions.AllowAny',
+        #'rest_framework.permissions.IsAuthenticated',
+         'rest_framework.permissions.AllowAny',
     ),
     # 'DEFAULT_RENDERER_CLASSES': (
     #     'rest_framework.renderers.JSONRenderer',
     # ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 # swagger config
