d5125551182cec12537a6c6deb7d907a550b01b8
DanielSanchez052 <sanchezd0528@gmail.com>
2023-08-12 16:33:29 -0500

    add captain definition to automatic deploy

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,4 +1,8 @@
 # python manage.py flush --no-input
 # python manage.py migrate
 
+python manage.py collectstatic --noinput
+python manage.py migrate --noinpu
 gunicorn --config gunicorn-cfg.py NotificationApi.wsgi
+
+# exec "$@"
