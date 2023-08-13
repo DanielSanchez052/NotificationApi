39fe907ca5979ae4c48576f6733b897ea49d9692
DanielSanchez052 <sanchezd0528@gmail.com>
2023-08-12 22:24:29 -0500

    fix noimput

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 # exec python manage.py flush --no-input
 # exec python manage.py migrate --noinput
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 
 exec gunicorn --config gunicorn-cfg.py NotificationApi.wsgi
