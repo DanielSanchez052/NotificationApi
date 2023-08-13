8f4b6fa31337e18ec38e20f30724624512bc4b31
DanielSanchez052 <sanchezd0528@gmail.com>
2023-08-13 00:44:27 -0500

    add permissions to staticfiles dir

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# exec python manage.py flush --no-input
-# exec python manage.py migrate --noinput
-
 python manage.py collectstatic --no-input
 
 exec gunicorn --config gunicorn-cfg.py NotificationApi.wsgi
