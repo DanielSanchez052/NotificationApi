b6eac07912a3b9526b749336a1677faf79944537
DanielSanchez052 <sanchezd0528@gmail.com>
2023-08-12 21:37:06 -0500

    remove migrate and flush in start

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-exec python manage.py flush --no-input
-exec python manage.py collectstatic --noinput
+# exec python manage.py flush --no-input
 # exec python manage.py migrate --noinput
+
+exec python manage.py collectstatic
+
 exec gunicorn --config gunicorn-cfg.py NotificationApi.wsgi
