0a2003c78e3722851ef764278e044a7564ce50aa
DanielSanchez052 <sanchezd0528@gmail.com>
2023-08-12 22:47:46 -0500

    change python version to 3.10

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 # exec python manage.py flush --no-input
 # exec python manage.py migrate --noinput
 
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 
 exec gunicorn --config gunicorn-cfg.py NotificationApi.wsgi
