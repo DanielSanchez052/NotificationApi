169b3bacbde0f22d7a271700ffe0cfe8085490cc
DanielSanchez052 <sanchezd0528@gmail.com>
2023-08-12 20:56:03 -0500

    add entrypoint and run script

--- start.sh
+++ start.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-
 exec python manage.py flush --no-input
 exec python manage.py collectstatic --noinput
 exec python manage.py migrate --noinput
-
 exec gunicorn --config gunicorn-cfg.py NotificationApi.wsgi
-
-# exec "$@"
