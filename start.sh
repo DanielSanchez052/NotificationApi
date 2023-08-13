3ba32b9810ad2242d65a8e34ed0808adc4527cdf
DanielSanchez052 <sanchezd0528@gmail.com>
2023-08-12 21:58:22 -0500

    fix start script

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 # exec python manage.py flush --no-input
 # exec python manage.py migrate --noinput
 
-exec python manage.py collectstatic
+python manage.py collectstatic --no-input
 
 exec gunicorn --config gunicorn-cfg.py NotificationApi.wsgi
